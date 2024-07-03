7a5d09ffcf0dd760e5fa3b72eb01cda71add5071
malla-lokesh <121810307046@gitam.in>
2024-07-03 19:25:58 +0530

    new: done with creation of todo and update status of todo

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,95 @@
-import React from 'react'
+'use client'
+
+import { Todo } from '@/payload-types'
+import React, { useEffect, useState } from 'react'
 
 const page = () => {
-  return <div>Welcome Page</div>
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [newTodo, setNewTodo] = useState('')
+
+  useEffect(() => {
+    const getTodos = async () => {
+      try {
+        const response = await fetch('/api/todos')
+        const todoData = await response.json()
+        setTodos(todoData.docs)
+      } catch (error) {}
+    }
+    getTodos()
+  }, [])
+
+  const handleTodoStatus = async (todo: Todo) => {
+    try {
+      const req = await fetch(`/api/todos/${todo?.id}`, {
+        method: 'PATCH',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          completed: !todo?.completed,
+        }),
+      })
+      const data = await req.json()
+      console.log(data)
+      const newTodos = todos?.map((currTodo) => {
+        if (currTodo.id === data.doc.id) {
+          currTodo.completed = data?.doc?.completed
+        }
+        return currTodo
+      })
+      setTodos(newTodos)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleTodoEdit = (todo: Todo) => {}
+
+  const handleCreateNewTodo = async () => {
+    try {
+      const req = await fetch('/api/todos', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          task: newTodo,
+        }),
+      })
+      const data = await req.json()
+      console.log(data)
+      setTodos((prevTodos) => [...prevTodos, data.doc])
+      setNewTodo('')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <div>Todos</div>
+      <input type="todo" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+      <button type="button" onClick={handleCreateNewTodo}>
+        Create new todo
+      </button>
+      {todos?.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo?.completed!}
+              onChange={() => handleTodoStatus(todo)}
+            />
+            <span onClick={() => handleTodoEdit(todo)} id="todo">
+              {todo?.task}
+            </span>
+          </div>
+        )
+      })}
+    </React.Fragment>
+  )
 }
 
 export default page
